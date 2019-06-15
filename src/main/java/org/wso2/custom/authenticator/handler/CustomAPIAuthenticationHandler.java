124e0c34b47686c4fcd9733e680971113b701dfd
GDRDABARERA <gdrdabarera@gmail.com>
2019-06-15 22:58:34 +0530

    removed unwanted inport

--- src/main/java/org/wso2/custom/authenticator/handler/CustomAPIAuthenticationHandler.java
+++ src/main/java/org/wso2/custom/authenticator/handler/CustomAPIAuthenticationHandler.java
@@ -1,6 +1,6 @@
 package org.wso2.custom.authenticator.handler;
 
-import com.sun.jna.StringArray;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.rest.AbstractHandler;
